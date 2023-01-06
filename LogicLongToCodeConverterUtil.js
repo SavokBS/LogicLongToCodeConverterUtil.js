9a8507f778341ec8e1081eb9b90f1656c8424539
Nikita Tvorogov <88921908+SavokBS@users.noreply.github.com>
2023-01-06 20:08:57 +0500

    Update LogicLongToCodeConverterUtil.js

--- LogicLongToCodeConverterUtil.js
+++ LogicLongToCodeConverterUtil.js
@@ -1,20 +1,37 @@
-function ToId(hashtag) {
+const Long = require('long');
+var tagChars = '0289PYLQGRJCUV';
 
-  let TagChar = ["0", "2", "8", "9", "P", "Y", "L", "Q", "G", "R", "J", "C", "U", "V"]
-  hashtag = hashtag.replace("#", "")
-  let TagArray = hashtag.toUpperCase().split('');
-  let ID = 0
-  for(let i=0; i<TagArray.length; i++) {
-    let char = TagArray[i];
-    let charIndex = TagChar.indexOf(char)
-    ID *= TagChar.length
-    ID += charIndex
+module.exports = {
+  tag2id: (tag) => {
+    if (tag === undefined || typeof tag !== 'string') return false;
 
+    let id = 0;
+    let tagArray = tag.split('');
+    for (let a = 0; a < tagArray.length; a++) {
+      let i = tagChars.indexOf(tagArray[a]);
+      id *= 14;
+      id += i
+    }
+    let high = id % 256;
+    // let low = (id - high) >>> 8
+    let low = Long.fromNumber((id - high)).shiftRight(8).low;
+    return {
+      high: high,
+      low: low
+    }
+  },
+
+  id2tag: (high, low) => {
+    // let id = ((low << 8) >>> 0) + high
+    let id = parseInt(Long.fromNumber(low).shiftLeft(8).shiftRight(0).toString()) + high;
+    let i = 0;
+    let tag = [];
+    while (id > 0) {
+      i = id % 14;
+      id = Math.floor(id / 14);
+      tag.push(tagChars.charAt(i))
+    }
+
+    return tag.reverse().join('')
   }
-  let hlid = []
-  hlid.push(ID % 256)
-  hlid.push((ID - hlid[0]) / 256)
-  return hlid
-
-
-}
+};
